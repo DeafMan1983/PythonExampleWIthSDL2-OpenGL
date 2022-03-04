c1a4b718ce4a61ea93acece907a4bc0a5b1603f8
DeafMan1983 <57066679+DeafMan1983@users.noreply.github.com>
2022-03-04 11:19:23 +0100

    Update 14_OpenGLonTkinter.py

--- Examples/Tkinter/14_OpenGLonTKinter ( Modern OpenGL )/14_OpenGLonTkinter.py
+++ Examples/Tkinter/14_OpenGLonTKinter ( Modern OpenGL )/14_OpenGLonTkinter.py
@@ -1,28 +1,67 @@
 import time
 import tkinter
-from OpenGL import GL
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+import numpy as np
 from pyopengltk import OpenGLFrame
 
 class AppOgl(OpenGLFrame):
 
     def initgl(self):
         """Initalize gl states when the frame is created"""
-        GL.glViewport(0, 0, self.width, self.height)
-        GL.glClearColor(1.0, 0.25, 0.0, 1.0)
-        self.start = time.time()
-        self.nframes = 0
+        glViewport(0, 0, self.width, self.height)
+        glClearColor(1.0, 0.25, 0.0, 1.0)
 
     def redraw(self):
         """Render a single frame"""
-        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
-        tm = time.time() - self.start
-        self.nframes += 1
-        print("fps",self.nframes / tm, end="\r" )
+        glClear(GL_COLOR_BUFFER_BIT)
+        # Start vertex and fragment sahders
+        VERTEX_SHADER = """#version 330
+            in vec4 position;
+            void main()
+            {
+                gl_Position = position;
+            }"""
+        
+        
+        
+        FRAGMENT_SHADER = """#version 330
+        
+            void main()
+            {
+                gl_FragColor = vec4(1.0f, 1.0f, 1.0f,1.0f);
+            }"""
 
+        # What is fuck with compileShader and compileProgram - It looks cheating!!!
+        # Please use original Modern OpenGL why not use hlCreateShader, glShaderSource and 
+        # glCreateProgram, glAttachShader, glCompileSHader and glLinkProgram  
+        vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
+        fragmentshader = shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
+        shaderProgram = shaders.compileProgram(vertexshader, fragmentshader)
+    
+        triangles = [-0.5, -0.5, 0.0,
+                    0.5, -0.5, 0.0,
+                    0.0, 0.5, 0.0]
+    
+        triangles = np.array(triangles, dtype=np.float32)
+    
+        VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)
+        glBufferData(GL_ARRAY_BUFFER, triangles.nbytes, triangles, GL_STATIC_DRAW)
+    
+        position = glGetAttribLocation(shaderProgram, 'position')
+        glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(position)
+
+        glUseProgram(shaderProgram)
+  
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+    
+        glUseProgram(0)
 
 if __name__ == '__main__':
     root = tkinter.Tk()
-    root.title("OpenGL on Tkinter")
+    root.title("Modern OpenGL on Tkinter")
     app = AppOgl(root, width=480, height=320)
     app.pack(fill=tkinter.BOTH, expand=tkinter.YES)
     app.animate = 1
